ab92e4f52144c71b18aa20170d464a127b9a34fe
AyushSahay1902 <sahayayush385@gmail.com>
2023-09-29 10:27:50 +0530

    first commit

---
+++ hello.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	var score int = 4
+// 	fmt.Println(score)
+// }
+
+func main() {
+	var batman string = "I am a Batman!"
+	fmt.Println(batman)
+	var superman string
+	superman = "i am SuperMan"
+	fmt.Println(superman)
+	thor := "I am Thor"
+	fmt.Println(thor)
+
+	var (
+		spiderman = "I am Spiderman"
+		age       = 18
+		powers    = "Web Building"
+	)
+	fmt.Println(spiderman, age, powers)
+}
